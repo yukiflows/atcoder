81412169ac85e6807e4264f82dc7394de1881741
yukiflows <yukiflows@gmail.com>
2022-02-03 16:45:02 +0000

    [solve] 競プロ典型90問 008 - AtCounter

---
+++ other-contests/競プロ典型90問/008.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+#define unique(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using vi  = vector<int>;
+using vl  = vector<long>;
+using vll = vector<long long>;
+using vs  = vector<string>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long>>;
+using vvll = vector<vector<long long>>;
+
+const long long INF = 1e9+7;
+const long long MOD = 1e9+7;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+int main() {
+    ll N; cin >> N;
+    string S; cin >> S;
+    string atcoder = "atcoder";
+
+    vl dp(7, 0);
+    rep(i, N) {
+        rep(j, 7) {
+            if(S[i] == atcoder[j]) {
+                if(j == 0) dp[j]++;
+                else dp[j] += dp[j-1];
+                dp[j] %= MOD;
+            }
+        }
+    }
+    cout << dp[6] << endl;
+
+    return 0;
+}
