4577aaab08506182e3fc2b2aed3e77393546ecbc
yukiflows <yukiflows@gmail.com>
2022-02-03 13:23:37 +0000

    [add] macros

--- template.cpp
+++ template.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-int main(void){
-    std::cout << "hello, world" << std::endl;
+#define pb push_back
+#define mp make_pair
+#define all(v) (v).begin() (v).end()
+#define unique(v) v.erase(unique(v.begin(), v.end()), v.end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using vi  = vector<int>;
+using vl  = vector<long>;
+using vll = vector<long long>;
+using vs  = vector<string>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long>>;
+using vvll = vector<vector<long long>>;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+int main() {
     return 0;
 }
