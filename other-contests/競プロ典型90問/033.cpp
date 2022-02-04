8a9bc2aa8b670074b92fef5fcc44c889e5479996
yukiflows <yukiflows@gmail.com>
2022-02-04 17:16:36 +0000

    [solve] 競プロ典型90問 033 - Not Too Bright

---
+++ other-contests/競プロ典型90問/033.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define all(v)  (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define unique(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using vi  = vector<int>;
+using vl  = vector<long>;
+using vll = vector<long long>;
+using vs  = vector<string>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long>>;
+using vvll = vector<vector<long long>>;
+
+const long long INF = 1e9+7;
+const long long MOD = 1e9+7;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+int main() {
+    int H, W; cin >> H >> W;
+    if(H == 1) cout << W << endl;
+    else if(W == 1) cout << H << endl;
+    else cout << ((H+1) / 2) * ((W+1) / 2) << endl;
+    return 0;
+}
