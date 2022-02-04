6a5c1b0d0b889e7a0f2e9d72dbb7689c81a2a9dd
yukiflows <yukiflows@gmail.com>
2022-02-04 12:49:05 +0000

    [solve] 競プロ典型90問 020 - Log Inequality

---
+++ other-contests/競プロ典型90問/020.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define all(v)  (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define unique(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using vi  = vector<int>;
+using vl  = vector<long>;
+using vll = vector<long long>;
+using vs  = vector<string>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long>>;
+using vvll = vector<vector<long long>>;
+
+const long long INF = 1e9+7;
+const long long MOD = 1e9+7;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+int main() {
+    ll a, b, c; cin >> a >> b >> c;
+    ll p = 1;
+    rep(i, b) p *= c;
+    
+    if(a < p) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
