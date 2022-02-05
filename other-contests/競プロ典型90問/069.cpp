5cb4a033bbce831bf91cef0659fbc21e54c5f09c
yukiflows <yukiflows@gmail.com>
2022-02-05 16:27:16 +0000

    [solve] 競プロ典型90問 069 - Colorful Blocks 2

---
+++ other-contests/競プロ典型90問/069.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+
+#define all(v)  (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define unique(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using pi  = pair<int, int>;
+using pl  = pair<long long, long long>;
+
+using vi  = vector<int>;
+using vl  = vector<long long>;
+using vs  = vector<string>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long long>>;
+
+using vvvi = vector<vector<vector<int>>>;
+using vvvl = vector<vector<vector<long long>>>;
+
+const long long INF = 1e9+7;
+const long long MOD = 1e9+7;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+ll modpow(ll a, ll x) {
+    ll ret = 1;
+    ll p = a;
+    while(x > 0) {
+        if(x & 1) ret *= p;
+        x >>= 1;
+        p = p*p;
+        ret %= MOD;
+        p %= MOD;
+    }
+    return ret;
+}
+
+int main() {
+    ll N, K; cin >> N >> K;
+    if(N == 1) cout << K << endl;
+    else cout << K * (K-1) % MOD * modpow(K-2, N-2) % MOD << endl;
+    return 0;
+}
