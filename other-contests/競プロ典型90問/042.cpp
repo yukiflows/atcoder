66ca8a28e27e58d1ab60e4bf3481550c7f1e4fe0
yukiflows <yukiflows@gmail.com>
2022-02-05 04:11:06 +0000

    [solve] 競プロ典型90問 042 - Multiple of 9

---
+++ other-contests/競プロ典型90問/042.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define all(v)  (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define unique(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using vi  = vector<int>;
+using vl  = vector<long>;
+using vll = vector<long long>;
+using vs  = vector<string>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long>>;
+using vvll = vector<vector<long long>>;
+
+const long long INF = 1e9+7;
+const long long MOD = 1e9+7;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+int main() {
+    ll K; cin >> K;
+    vl dp(K+100, 0);
+    dp[0] = 1;
+    rep(i, K) {
+        reps(j, 9) {
+            dp[i+j] += dp[i];
+            dp[i+j] %= MOD;
+        }
+    }
+    if(K % 9 != 0) cout << 0 << endl;
+    else cout << dp[K] << endl;
+    return 0;
+}
