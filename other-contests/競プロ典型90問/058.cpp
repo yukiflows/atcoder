c0a35eb3d8e79dc8e46cab9715503ec17e9359f1
yukiflows <yukiflows@gmail.com>
2022-02-05 13:56:36 +0000

    [solve] 競プロ典型90問 058 - Original Calculator

---
+++ other-contests/競プロ典型90問/058.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+
+#define all(v)  (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define unique(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using pi  = pair<int, int>;
+using pl  = pair<long long, long long>;
+
+using vi  = vector<int>;
+using vl  = vector<long long>;
+using vs  = vector<string>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long long>>;
+
+using vvvi = vector<vector<vector<int>>>;
+using vvvl = vector<vector<vector<long long>>>;
+
+const long long INF = 1e9+7;
+const long long MOD = 1e9+7;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+int main() {
+    ll N, K; cin >> N >> K;
+    vl a(100010, 0);
+    ll c = 0;
+    while(K > 0) {
+        ll s = 0; 
+        ll x = N;
+        while(x > 0) {
+            s += x % 10;
+            x /= 10;
+        }
+        ll y = N + s; 
+        ll z = y % 100000;
+        N = z; 
+        c++;
+        K--;
+        if(a[N] != 0) K %= c-a[N];
+        a[N] = c;
+    }
+    cout << N << endl;
+    return 0;
+}
