4c32cbb2fc47b0ad18efae4981cbfe366310e9ac
yukiflows <yukiflows@gmail.com>
2022-02-06 03:14:02 +0000

    [solve] 競プロ典型90問 075 - Magic For Balls

---
+++ other-contests/競プロ典型90問/075.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+
+#define all(v)  (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define unique(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using pi  = pair<int, int>;
+using pl  = pair<long long, long long>;
+
+using vi  = vector<int>;
+using vl  = vector<long long>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long long>>;
+
+using vvvi = vector<vector<vector<int>>>;
+using vvvl = vector<vector<vector<long long>>>;
+
+const long long INF = 1e9+7;
+const long long MOD = 1e9+7;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+int main() {
+    ll N; cin >> N;
+
+    bool is_prime = true;
+
+    ll r = N, c = 0;
+    for(ll i = 2; i*i <= N; i++) {
+        while(r % i == 0) {
+            r /= i;
+            c++;
+            is_prime = false;
+        }
+    }
+    if(r != 1) c++;
+
+    if(is_prime) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    ll ans = 0;
+    ll d = 1;
+    while(d < c) {
+        ans++;
+        d *= 2;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
