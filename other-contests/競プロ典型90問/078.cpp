742d0cee3ba26475be57fa971f4586387accedce
yukiflows <yukiflows@gmail.com>
2022-02-06 06:35:07 +0000

    [solve] 競プロ典型90問 078 - Easy Graph Problem

---
+++ other-contests/競プロ典型90問/078.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+
+#define all(v)  (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define unique(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+
+#define rep(i, n)   for(int i = 0; i < (n); i++) 
+#define reps(i, n)  for(int i = 1; i <= (n); i++)
+#define rrep(i, n)  for(int i = (n)-1; i > 0; i--)
+#define rreps(i, n) for(int i = (n); i >= 0; i--)
+
+using namespace std;
+
+using ll = long long;
+
+using pi  = pair<int, int>;
+using pl  = pair<long long, long long>;
+
+using vi  = vector<int>;
+using vl  = vector<long long>;
+using vs  = vector<string>;
+
+using vvi  = vector<vector<int>>;
+using vvl  = vector<vector<long long>>;
+
+using vvvi = vector<vector<vector<int>>>;
+using vvvl = vector<vector<vector<long long>>>;
+
+const long long INF = 1e9+7;
+const long long MOD = 1e9+7;
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if(a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if(a < b) a = b; return compare;}
+
+int main() {
+    int N, M; cin >> N >> M;
+    vvi G(N);
+    rep(i, M) {
+        int a, b; cin >> a >> b;
+        a--; b--;
+        G[a].pb(b);
+        G[b].pb(a);
+    }
+
+    int ans = 0;
+    rep(i, N) {
+        int c = 0;
+        for(int j: G[i]) {
+            if(j < i) c++;
+        }
+        if(c == 1) ans++;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
